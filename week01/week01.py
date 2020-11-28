3226bef6dd93dc6524c5cc8b9f89642a54938055
JQLiao <349532402@qq.com>
2020-11-28 22:01:30 +0800

    week01 homework

---
+++ week01/week01.py
@@ -0,0 +1,34 @@
+import time
+import datetime
+import logging
+import os
+
+
+def week01():
+    # 获取当期日期
+    now = datetime.date.today()
+
+    # 日志绝对路径
+    logPath = 'C:\\JQL\\study\\log\\python-{}\\week01.log'.format(now)
+    #basePath = '/var/log/python-{}/week01.log'.format(datetime.date.today())
+    # 日志所在目录
+    logDir = os.path.dirname(logPath)
+    if not os.path.exists(logDir):
+        os.makedirs(logDir)
+    
+    logging.basicConfig(filename=logPath,
+                        format='%(asctime)s - %(levelname)s - [line: %(levelno)d ] %(message)s',
+                        datefmt='%Y/%m/%d %H:%M:%S',
+                        level=logging.INFO)
+    
+    logging.info('调用程序啦啦啦~')
+
+if __name__ == '__main__':
+    while True:
+        week01()
+        time.sleep(2)
+
+
+
+
+
