e3ceac67edc062677d494a9b103cfa08b6006b1a
JQLiao <349532402@qq.com>
2020-12-06 18:36:24 +0800

    zhihu crawl

---
+++ week02/zhihu_crawl.py
@@ -0,0 +1,34 @@
+import requests
+from lxml import etree
+from time import sleep
+import json
+from pathlib import Path
+import os
+
+downPath = Path('C:\\JQL\\download\\zhihu\\ans.txt')
+
+def get_zhihu_info(zhihuUrl):
+    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
+    header = {'user-agent':ua}
+    response = requests.get(zhihuUrl, headers=header)
+    data = json.loads(response.text)
+    return data["data"]
+
+def save_data(data):
+    with open(downPath, 'ab') as f:
+        for index,item in enumerate(data):
+            content = item["target"]["content"]
+            try:
+                f.write(content.encode('utf-8'))
+                f.write(b'\n=============================\n')
+            except Exception as e:
+                print(e)
+
+if __name__ == "__main__":
+    alldata = []
+    urls = tuple(f'https://www.zhihu.com/api/v4/topics/20775155/feeds/essence?include=data%5B%3F%28target.type%3Dtopic_sticky_module%29%5D.target.data%5B%3F%28target.type%3Danswer%29%5D.target.content%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%3Bdata%5B%3F%28target.type%3Dtopic_sticky_module%29%5D.target.data%5B%3F%28target.type%3Danswer%29%5D.target.is_normal%2Ccomment_count%2Cvoteup_count%2Ccontent%2Crelevant_info%2Cexcerpt.author.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B%3F%28target.type%3Dtopic_sticky_module%29%5D.target.data%5B%3F%28target.type%3Darticle%29%5D.target.content%2Cvoteup_count%2Ccomment_count%2Cvoting%2Cauthor.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B%3F%28target.type%3Dtopic_sticky_module%29%5D.target.data%5B%3F%28target.type%3Dpeople%29%5D.target.answer_count%2Carticles_count%2Cgender%2Cfollower_count%2Cis_followed%2Cis_following%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B%3F%28target.type%3Danswer%29%5D.target.annotation_detail%2Ccontent%2Chermes_label%2Cis_labeled%2Crelationship.is_authorized%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Canswer_type%3Bdata%5B%3F%28target.type%3Danswer%29%5D.target.author.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B%3F%28target.type%3Danswer%29%5D.target.paid_info%3Bdata%5B%3F%28target.type%3Darticle%29%5D.target.annotation_detail%2Ccontent%2Chermes_label%2Cis_labeled%2Cauthor.badge%5B%3F%28type%3Dbest_answerer%29%5D.topics%3Bdata%5B%3F%28target.type%3Dquestion%29%5D.target.annotation_detail%2Ccomment_count%3B&limit=5&offset={page * 5}' for page in range(3))
+    for zhihuUrl in urls:
+        data = get_zhihu_info(zhihuUrl)
+        alldata = alldata + data
+    save_data(alldata)
+        
