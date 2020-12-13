46de44e2eaad1fbe58bfa0fe2ff1c2e269ccac03
JQLiao <349532402@qq.com>
2020-12-13 12:20:48 +0800

    本周作业

--- week03/num06.py
+++ week03/num06.py
@@ -16,6 +16,7 @@ password = dbserver["password"]
 Base = declarative_base()
 
 class User(Base):
+    """用户表"""
     __tablename__ = "user"
     id = Column(Integer(), primary_key=True)
     user_name = Column(String(15), unique=True)
@@ -27,6 +28,7 @@ class User(Base):
         return result
 
 class Account(Base):
+    """资产表"""
     __tablename__ = "account"
     id = Column(Integer(), primary_key=True)
     user_id = Column(Integer())
@@ -39,6 +41,7 @@ class Account(Base):
         return result
 
 class AccountFlow(Base):
+    """审计表"""
     __tablename__ = "account_flow"
     id = Column(Integer(), primary_key=True)
     from_id = Column(Integer())
@@ -51,8 +54,10 @@ class AccountFlow(Base):
         result = '{"from_id":%s, "to_id":%s, "deal_money":%s}' % (self.user_id, self.user_asset, self.deal_money)
         return  result
 
+#建立连接
 dburl = "mysql+pymysql://%s:%s@%s:3306/%s?charset=utf8mb4" %(user,password,host,database)
 engine = create_engine(dburl, echo=True, encoding="utf-8")
+
 Base.metadata.create_all(engine)
 
 #增加测试数据
@@ -66,17 +71,22 @@ session.commit()
 
 with engine.begin() as conn:
     zhangsan_count = session.query(Account).outerjoin(User, Account.user_id==User.id).filter(User.user_name == "张三").first()
-    lisi_count = session.query(Account).outerjoin(User, Account.user_id==User.id).filter(User.user_name == "李四").first(
+    lisi_count = session.query(Account).outerjoin(User, Account.user_id==User.id).filter(User.user_name == "李四").first()
   
     if float(zhangsan_count.user_asset) >= 100:
+        sql = "UPDATE account SET user_asset=%s, update_time=%s WHERE user_id=%s"
         #张三账户减100
-        conn.execute("UPDATE account SET user_asset=%s WHERE user_id=%s" %(float(zhangsan_count.user_asset)-100, int(zhangsan_count.user_id)))
+        zs_value = (float(zhangsan_count.user_asset)-100, str(datetime.now()), int(zhangsan_count.user_id))
+        conn.execute(sql,zs_value)
+       
         #李四账户增100
-        conn.execute("UPDATE account SET user_asset=%s WHERE user_id=%s" %(float(lisi_count.user_asset)+100, int(lisi_count.user_id)))
-        
+        ls_value = (float(lisi_count.user_asset)+100, str(datetime.now()), int(lisi_count.user_id))
+        conn.execute(sql,ls_value)
+
         #审计表记录交易
         sql = "INSERT INTO account_flow(from_id, to_id, deal_money, created_time, update_time) VALUES(%s,%s,%s,%s,%s)" 
-        value = (int(zhangsan_count.user_id),int(lisi_count.user_id),300,str(datetime.now(),str(datetime.now()))
+        value = (int(zhangsan_count.user_id),int(lisi_count.user_id),100,str(datetime.now()),str(datetime.now()))
         conn.execute(sql,value)
     else:
         print("余额不足，交易失败")
+        conn.execute("UPDATE account SET user_asset=%s WHERE user_id=%s" %(float(zhangsan_count.user_asset)+200, int(zhangsan_count.user_id)))
